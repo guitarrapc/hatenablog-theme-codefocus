5c5dde2dbf908b4bb95760e9c3ab038e69ec06fa
Ikiru Yoshizaki <3856350+guitarrapc@users.noreply.github.com>
2025-05-15 02:49:51 +0900

    chore: indent

--- vite.config.js
+++ vite.config.js
@@ -2,21 +2,21 @@ import { defineConfig } from "vite";
 import autoprefixer from "autoprefixer";
 
 export default defineConfig({
-    build: {
-        rollupOptions: {
-            input: ["scss/boilerplate.scss", "js/toc-button.js", "js/toc-toggle.js"],
-            output: {
-                assetFileNames: ({ name }) => name,
-                entryFileNames: 'js/[name].js',
-            },
-        },
-        outDir: "build",
-        cssMinify: false,
+  build: {
+    rollupOptions: {
+      input: ["scss/boilerplate.scss", "js/toc-button.js", "js/toc-toggle.js"],
+      output: {
+        assetFileNames: ({ name }) => name,
+        entryFileNames: 'js/[name].js',
+      },
     },
-    css: {
-        devSourcemap: true,
-        postcss: {
-            plugins: [autoprefixer()],
-        },
+    outDir: "build",
+    cssMinify: false,
+  },
+  css: {
+    devSourcemap: true,
+    postcss: {
+      plugins: [autoprefixer()],
     },
+  },
 });
